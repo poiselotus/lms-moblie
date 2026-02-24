b1b728be183e8476d94e9a0859f62d504f9bae58
poiselotus <mac4u2luv@gmail.com>
2026-02-24 21:44:32 +0100

    fix: update web background colors to match app theme

--- app/+html.tsx
+++ app/+html.tsx
@@ -29,10 +29,10 @@ export default function Root({ children }: { children: React.ReactNode }) {
 
 const responsiveBackground = `
 body {
-  background-color: #fff;
+  background-color: #F8F9FA;
 }
 @media (prefers-color-scheme: dark) {
   body {
-    background-color: #000;
+    background-color: #1A1A2E;
   }
 }`;
